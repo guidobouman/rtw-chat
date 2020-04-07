a5c268d0d4a680cf594e9dc3f2567f6a07835b6d
Guido <guido@q42.nl>
2020-04-07 08:12:01 +0200

    apply basic socket.io demo

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import http from 'http';
+import socketIO from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const ioInstance = socketIO(server);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, function(){
+ioInstance.on('connection', function(socket) {
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    ioInstance.emit('chat message', msg);
+  });
+})
+
+server.listen(3000, function() {
   console.log('listening on *:3000');
 });
