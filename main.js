260d7b6a614a157a8b0e346009fb04b8d48ec9a6
Guido <guido@q42.nl>
2020-04-17 10:44:17 +0200

    get trends from test data

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 import express from 'express';
 import http from 'http';
-import socketIO from 'socket.io';
+import ioServer from 'socket.io';
+import ioClient from 'socket.io-client';
 
 const app = express();
 const server = http.createServer(app);
-const ioInstance = socketIO(server);
+const ioInstance = ioServer(server);
+
+const historyLenght = 100;
+let lastAverage = 0;
+let eventLog = [];
+
+const externalData = ioClient('http://localhost:3001');
+externalData.on('event occured', function(event) {
+  eventLog.push(event);
+  eventLog = eventLog.slice(eventLog.length - historyLenght);
+
+  const total = eventLog.reduce((total, current) => {
+    return total + current.value;
+  }, 0);
+  const average = total / eventLog.length;
+
+  if(lastAverage < 500 && average > 500) {
+    console.log('now positive');
+    ioInstance.emit('server message', `SERVER: the trend has changed and is now positive`);
+  }
+  if(lastAverage > 500 && average < 500) {
+    console.log('now negative');
+    ioInstance.emit('server message', `SERVER: the trend has changed and is now negative`);
+  }
+
+  // ioInstance.emit('server message', `SERVER: the trend has changed ${average > 500 ? 'positive' : 'negative'}`);
+
+  // ioInstance.emit('average', `SERVER: the average right now is ${average > 500 ? 'positive' : 'negative'}`);
+  // ioInstance.emit('trend direction', `SERVER: the trend right now is going ${average > lastAverage ? 'up' : 'down'}`);
+
+  lastAverage = average;
+})
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
